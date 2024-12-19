d743a0126019a16ae2b7e318d1c5f06e63771e15
George Rodriguez <GRodriguez@ocde.us>
2024-12-18 16:32:47 -0800

    Add initial project files and configuration for ShinyPokemonBingoGenerator

---
+++ ShinyPokemonBingoGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ShinyPokemonBingoGenerator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
